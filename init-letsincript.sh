55adc3c044410eb963c5fb289c91f658d0d784eb
MateBook <examletru@gmail.com>
2022-04-03 17:23:40 +0300

    third commit

--- init-letsincript.sh
+++ init-letsincript.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(quicksales.online www.quicksales.online)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./services/certbot"
 email="a1kk3@mail.ru" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
